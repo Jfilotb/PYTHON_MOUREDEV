dd104ade9456cb3507467a6c16dab01f08c4dbac
JOFIBA79 <jenriquefilot@gmail.com>
2024-04-12 03:31:11 -0500

    Actualizando

---
+++ 00_hello.py
@@ -0,0 +1,28 @@
+#  Esto es un comentario:
+# Nuestro Hola mundo
+print("Hola Python")
+print('Hola Python')
+
+
+"""
+Este es un 
+comentario 
+en varias lineas
+"""
+
+
+'''
+Este tambien es un 
+coemntario
+en varias lineas
+'''
+
+# Consultar el tipo de dato
+print(type("Hola Python"))  # Tipo str
+print(type(5))  # Tipo int
+print(type(1.5))  # Tipo Float
+print(type(1 + 3j))  # Tipo Complex complejo
+print(type(True))  # Tipo Boolean
+
+
+print(type(print("Mi cadena de texto")))  # Tipo 'Nonetype'
