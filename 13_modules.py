8796ec8b22095968e6d02091a92907118b601030
Jfilotb <jenriquefilot@gmail.com>
2024-05-08 14:37:29 -0500

    Module

---
+++ 13_modules.py
@@ -0,0 +1,18 @@
+### MODULES ###
+
+
+import math
+from module import printValue, sum
+import module
+
+
+module.sum(5, 3, 1)
+module.printValue("Hola Johan")
+
+
+sum(6, 14, 20)
+printValue('Hola Johann')
+
+
+print(math.pi)
+print(math.pow(2, 8))
---
+++ module.py
@@ -0,0 +1,8 @@
+
+
+def sum(numberOne, numberTwo, numberThere):
+    print(numberOne + numberTwo + numberThere)
+
+
+def printValue(values):
+    print(values)
